75263dd1ae7d5f381c574ba0359653c21dd767bb
diegoyamasaki <yamasaki.diego@gmail.com>
2022-10-15 10:40:06 -0300

    schemas e criacao do banco

--- core/configs.py
+++ core/configs.py
@@ -3,9 +3,10 @@ from typing import List
 from pydantic import BaseSettings
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class Settings(BaseSettings):
-    API_V1_STR: str =  '/api/v1'
-    DB_URL: str = 'postgresql+asyncpg//postgres:admin@localhost:5432/faculdate'
+    API_V1_STR: str = '/api/v1'
+    DB_URL: str = 'postgresql+asyncpg://postgres:admin@localhost:5432/faculdade'
     DBBaseModel = declarative_base()
 
     JWT_SECRET: str = 'C394Eb5iosnH7ppFmcxO1Cyc1TA4D2Rjjop0ppevRCg'
